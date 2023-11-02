641cff13202cac4edc7a94381659d1f9633a9be5
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-02 10:48:49 -0300

    Add files via upload

---
+++ Property Wrappers/Property Wrappers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Property Wrappers
+//
+//  Created by Lucas Neves dos santos pompeu on 02/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Property Wrappers/Property Wrappers/Property_WrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Property_WrappersApp.swift
+//  Property Wrappers
+//
+//  Created by Lucas Neves dos santos pompeu on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Property_WrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
