3992d187a265f5ce1a07a20e29e5ef17fe576080
Lucas Neves <lucas.neves12@icloud.com>
2023-11-02 15:28:40 -0300

    Using property Wrapper @State in TextField.

--- Property Wrappers/Property Wrappers/ContentView.swift
+++ Property Wrappers/Property Wrappers/ContentView.swift
@@ -10,20 +10,29 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var exibirMensagem: Bool = false
+    @State private var label: String = ""
     
     var body: some View {
         VStack {
-            Text(exibirMensagem ? "Mundo" : "Olá")
-                .font(.system(size: 26, weight: .bold))
-            Button("Alterar mensagem") {
-                exibirMensagem.toggle()
+            VStack {
+                Text(exibirMensagem ? "Mundo" : "Olá")
+                    .font(.system(size: 26, weight: .bold))
+                Button("Alterar mensagem") {
+                    exibirMensagem.toggle()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundStyle(Color.white)
+                .cornerRadius(8)
+            }
+            VStack {
+                Text(label)
+                    .font(.system(size: 26, weight: .bold))
+                TextField("Digite um texto", text: $label)
+                    .textFieldStyle(.roundedBorder)
             }
             .padding()
-            .background(Color.blue)
-            .foregroundStyle(Color.white)
-            .cornerRadius(8)
         }
-        .padding()
     }
 }
 
